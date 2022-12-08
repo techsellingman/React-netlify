929a296368c1abfbf4a4b3f01ae2defb8102ef02
GreenEarth1019 <greenearth1019@gmail.com>
2022-12-08 10:02:59 -0500

    initial-commit

---
+++ src/components/layouts/Headerfour.js
@@ -0,0 +1,89 @@
+import React, { Fragment } from 'react';
+import Menu from '../layouts/Menu';
+import Mobilemenu from '../layouts/Mobilemenu';
+import HeaderComponent from '../../helper/Navigationhelper';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { Dropdown, NavLink } from 'react-bootstrap';
+class Headerfour extends HeaderComponent {
+    render() {
+        return (
+            <Fragment>
+                {/* Aside (Mobile Navigation) */}
+                <aside className={classNames("main-aside", { "open": this.state.navtoggle })}>
+                    <div className="aside-title">
+                        <div className="aside-controls aside-trigger">
+                            <h4>Menu</h4>
+                            <div className="close-btn close-dark" onClick={this.navtoggleClass} >
+                                <span />
+                                <span />
+                            </div>
+                        </div>
+                    </div>
+                    <Mobilemenu />
+                </aside>
+                <div className="aside-overlay aside-trigger" onClick={this.navtoggleClass} />
+                {/* Header Start */}
+                <header className="main-header">
+                    {/* Top Header Start */}
+                    <div className="top-header">
+                        <div className="container">
+                            <div className="top-header-inner">
+                                <ul className="social-media">
+                                    <li> <Link to="#"> <i className="fab fa-facebook-f" /> </Link> </li>
+                                    <li> <Link to="#"> <i className="fab fa-pinterest-p" /> </Link> </li>
+                                    <li> <Link to="#"> <i className="fab fa-linkedin-in" /> </Link> </li>
+                                    <li> <Link to="#"> <i className="fab fa-twitter" /> </Link> </li>
+                                </ul>
+                                <ul className="top-header-nav">
+                                    <li> <Link to="/login"> Login</Link> </li>
+                                    <li>or</li>
+                                    <li> <Link to="/register"> Signup</Link> </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Top Header End */}
+                    <nav className="navbar">
+                        <div className="container">
+                            {/* Menu */}
+                            <Menu />
+                            <div className="header-controls">
+                                <ul className="header-controls-inner d-none d-lg-flex">
+                                    <li>
+                                        <Link to="/submit-listing" className="btn-custom primary">Submit Listing <i className="fas fa-plus" /> </Link>
+                                    </li>
+                                </ul>
+                                {/* Toggler */}
+                                <div className="aside-toggler aside-trigger" onClick={this.navtoggleClass}>
+                                    <span />
+                                    <span />
+                                    <span />
+                                </div>
+                            </div>
+                            
+                            <div>
+                            <Dropdown className="acr-language-selector">
+                                <Dropdown.Toggle as={NavLink} className="dropdownicon">
+                                    <img src={process.env.PUBLIC_URL + "/assets/img/flags/united-states.png"} alt="flag" />
+                                    <span>English</span>
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu className="dropdown-menu">
+                                    <ul>
+                                        <li> <Link to="#"><img src={process.env.PUBLIC_URL + "/assets/img/flags/china.png"} alt="flag" /> Chinese</Link> </li>
+                                        <li> <Link to="#"><img src={process.env.PUBLIC_URL + "/assets/img/flags/united-states.png"} alt="flag" />English</Link> </li>
+                                        <li> <Link to="#"><img src={process.env.PUBLIC_URL + "/assets/img/flags/thailand.png"} alt="flag" /> Thai</Link> </li>
+                                    </ul>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+                {/* Header End */}
+            </Fragment>
+        );
+    }
+}
+
+export default Headerfour;
---
+++ src/components/pages/About.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react';
+import MetaTags from "react-meta-tags";
+import Header from '../layouts/Header';
+import Breadcrumb from '../layouts/Breadcrumb';
+// import Footer from '../layouts/Footer';
+import Content from '../sections/about/Content';
+import Footerthree from '../layouts/Footerthree';
+
+class About extends Component {
+    render() {
+        return (
+            <Fragment>
+                <MetaTags>
+                    <title>Acres - Real Estate React Template | About Us</title>
+                    <meta
+                        name="description"
+                        content="#"
+                    />
+                </MetaTags>
+                <Header/>
+                <Breadcrumb breadcrumb={{pagename:'About Us'}} />
+                <Content/>
+                <Footerthree/>
+            </Fragment>
+        );
+    }
+}
+
+export default About;
